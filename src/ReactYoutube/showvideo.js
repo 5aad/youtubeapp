bcdb8340423162fbf7d1cb25765753afde390f9a
Saad <saad_anwer@live.com>
2020-04-30 20:21:30 +0500

    ch

--- src/ReactYoutube/showvideo.js
+++ src/ReactYoutube/showvideo.js
@@ -9,9 +9,10 @@ class Showvideo extends Component {
       <div className="container">
         <div className="card">
           <div className="card-body">
-            <h3 className="card-title">Vide Title</h3>
             <a href="/playlist" >Playlist</a>
+            <div class="embed-responsive embed-responsive-16by9">
             <Reactyoutube  />
+            </div>
           </div>
         </div>
       </div>
