7be8ad8dbd04416e152f18994686c82b2417eda5
Saad <saad_anwer@live.com>
2020-04-30 20:09:09 +0500

    Updates

---
+++ precache-manifest.2f74f711e9970a8d3826e939bc12e5dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b38c7fc80c4c4826350adab6cb15175",
+    "url": "/youtubeapp/index.html"
+  },
+  {
+    "revision": "9d8fa14dae0336b06ea2",
+    "url": "/youtubeapp/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "cfaed1c0b6c06407c1d1",
+    "url": "/youtubeapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d8fa14dae0336b06ea2",
+    "url": "/youtubeapp/static/js/2.7100dc98.chunk.js"
+  },
+  {
+    "revision": "d48bb4deb9bc69d8e49f593901f16876",
+    "url": "/youtubeapp/static/js/2.7100dc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfaed1c0b6c06407c1d1",
+    "url": "/youtubeapp/static/js/main.76484e6a.chunk.js"
+  },
+  {
+    "revision": "372be474e24f235a395e",
+    "url": "/youtubeapp/static/js/runtime-main.cb680744.js"
+  }
+]);
